08fd6812f4567753881144b8ebeea96bd0a99854
adviro22_laptop <ajosueramos@gmail.com>
2024-05-13 13:06:55 -0500

    Subida de archivos

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import connection, { dbConfig } from "./db.js";
+
+//dotenv.config({ path: "./.env" });
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/resources", express.static("static"));
+app.use("/resources", express.static(import.meta.url + "/public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) =>{
+    res.render("index", {titulo: "Pagina de Inicio", nombre: "Josue Ortiz Ochoa"})
+})
+
+app.get("/contactame", (req, res) =>{
+    res.render("contactame", {titulo: "Contactame"})
+})
+
+app.listen(PORT, () => {
+    console.log("Pagina en: http://localhost:" + PORT);
+  });
